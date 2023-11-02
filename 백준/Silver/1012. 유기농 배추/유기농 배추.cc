4e39f66487e2203b2251db3a0c4d3fc0c14f2f6b
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2023-11-02 20:48:34 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,45 +1,60 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1}; 
-int m, n, k, y, x, ret, ny, nx, t;
-int a[51][51];
-bool visited[51][51]; 
-void dfs(int y, int x){
-    visited[y][x] = 1;
-    for(int i = 0; i < 4; i++){
-        ny = y + dy[i];
-        nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny >=n || nx >= m) continue;
-        if(a[ny][nx] == 1 && !visited[ny][nx]){
-            dfs(ny, nx);
-        }
-    }
-    return;
-}
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int a1[4]={1,-1,0,0};
+int b1[4]={0,0,1,-1};
 
-int main(){ 
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> t;
-    while(t--){
-        fill(&a[0][0], &a[0][0] + 51 * 51, 0);
-        fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
-        ret = 0;
-        cin >> m >> n >> k;
-        for(int i = 0; i < k; i++){
-            cin >> x >> y;
-            a[y][x] = 1;
-        }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(a[i][j] == 1 && !visited[i][j]){
-                    dfs(i, j);
-                    ret++;
-                }
-            }
-        }
-        cout << ret << "\n"; 
+int M,N,K;
+
+int arr[51][51];
+bool visited[51][51];
+ 
+void dfs(int a, int b){  //a세로, b가로 
+	visited[a][b] = true;
+
+    for(int i = 0 ; i < 4 ; i++){
+    	int res1 = a + a1[i]; //세로 
+    	int res2 = b + b1[i]; //가로 
+    	
+        if(res2 < 0 || res2 >= M || res1 < 0 || res1 >= N) 
+            continue;
+  		
+  		if(arr[res1][res2] == 1){
+  			if(!visited[res1][res2]){
+  				dfs(res1, res2);
+			}
+		}
     }
+}
+ 
+int main(){
+    int T;
+    cin >> T;
+    
+    while(T--){
+    	cin >> M >> N >> K;
+    	int count = 0;
+    	for(int i = 0 ; i < K ; i++){
+    		int X, Y;
+    		cin >> X >> Y;
+    		arr[Y][X] = 1;
+		}
+    	
+    	for(int i = 0 ; i < N ; i++){
+    		for(int j = 0 ; j < M ; j++){
+    			if(arr[i][j] == 1){
+    				if(!visited[i][j]){
+    					count++;
+    					dfs(i, j);
+					}
+				}
+			}
+		}
+		cout << count << "\n";
+		memset(visited, false, sizeof(visited));
+		memset(arr, 0 , sizeof(arr));
+    	
+	}
+    
     return 0;
 }
