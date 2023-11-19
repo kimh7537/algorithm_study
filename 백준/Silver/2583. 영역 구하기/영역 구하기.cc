4293c9a2bcaeb14aa6ed7f87160b6664e3687b44
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2023-11-19 20:48:05 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,49 +1,52 @@
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
-#define y1 aaaa 
-int a[104][104], M, N, K, x1, x2, y1, y2, visited[104][104];
-const int dy[4] = {-1, 0, 1, 0};
-const int dx[4] = {0, 1, 0, -1};
-vector<int> ret; 
+int M, N, K, num, cnt;
+int arr[104][104], dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
+vector<int> v;
 
-int dfs(int y, int x){
-    visited[y][x] = 1; 
-    int ret = 1; 
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i]; 
-        int nx = x + dx[i]; 
-        if(ny < 0 || ny >= M || nx < 0 || nx >= N || visited[ny][nx] == 1) continue; 
-		if(a[ny][nx] == 1) continue;
-        ret += dfs(ny, nx);
-    } 
-    return ret; 
+void dfs(int y, int x){
+	arr[y][x] = 1;
+	num++;
+	for(int i = 0 ; i < 4 ; i++){
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if(ny < 0 || nx < 0 || ny >= M || nx >= N) continue;
+		if(arr[ny][nx] == 0){
+			dfs(ny, nx);
+		}
+	}
 }
 
-int main() { 
-    ios_base::sync_with_stdio(false);  
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    cin >> M >> N >> K; 
-    for(int i = 0; i < K; i++){
-        cin >> x1 >> y1 >> x2 >> y2;  
-        for(int i = x1; i < x2; i++){
-            for(int j = y1; j < y2; j++){
-                a[j][i] = 1; 
-            }
-        }
-    }
-    
-    for(int i = 0; i < M; i++){
-        for(int j = 0; j < N; j++){
-            if(a[i][j] != 1 & visited[i][j] == 0) { 
-                ret.push_back(dfs(i, j));
-            }
-        }
-    }
-    sort(ret.begin(), ret.end());
-    cout << ret.size() << "\n";
-    for(int a : ret) cout << a << " "; 
-    return 0; 
+int main(){
+	cin >> M >> N >> K;
+	
+	while(K--){
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		
+		for(int i = y1 ; i < y2 ; i++){
+			for(int j = x1 ; j < x2 ; j++){
+				arr[i][j] = 1;
+			}
+		}
+	}
+	
+	for(int i = 0 ; i < M ; i++){
+		for(int j = 0 ; j < N ; j++){
+			if(arr[i][j] == 0){
+				num = 0;
+				dfs(i, j);
+				v.push_back(num);
+				cnt++;
+			}
+		}
+	}
+	sort(v.begin(), v.end(), less<int>());
+	cout << cnt << "\n";
+	for(int i = 0 ; i < v.size() ; i++){
+		cout << v[i] << " ";
+	}
+	
+	return 0;
 }
