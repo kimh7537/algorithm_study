08f01c0fb2b74417045657a353f99d4351f64741
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2023-12-01 02:30:52 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -4,46 +4,44 @@ using namespace std;
 string s;
 vector<char> v = {'a', 'e', 'i', 'o', 'u'};
 
+
 int main(){
 	
 	while(cin >> s){
 		if(s == "end") break;
 		
-//		bool flag = false;
-		bool is_include_v = 0;
-		bool flag = true;
-			
+		bool flag = false;
+		
 		for(int i = 0 ; i < s.size() ; i++){
 			auto a = find(v.begin(), v.end(), s[i]);
 			auto b = find(v.begin(), v.end(), s[i+1]);
 			auto c = find(v.begin(), v.end(), s[i+2]);
-		
-
-			if(a != v.end()) is_include_v = 1;
 			
+			if(a != v.end()){
+				flag = true;
+			}
 			if(i + 2 < s.size()){
 				if(a != v.end() && b != v.end() && c != v.end()) {
-					flag = false;
+					flag = false; break;
 				}
 				if(a == v.end() && b == v.end() && c == v.end()){
-					flag = false;
+					flag = false; break;
 				}
 			}
 			if(i + 1 < s.size()){
 				if(s[i] == s[i+1]){
 					if(s[i] != 'e' && s[i] != 'o'){
-						flag = false;
+						flag = false; break;
 					}
 				}
 			}
 		}
 		
-		if(is_include_v == 0) flag = false; 
 		if(flag) cout << "<" << s << ">" << " is acceptable." << "\n";
 		else if(flag == false) cout << "<" << s << ">" << " is not acceptable." << "\n";
+		
 	}
+	
 	
 	return 0;
 }
-
-
