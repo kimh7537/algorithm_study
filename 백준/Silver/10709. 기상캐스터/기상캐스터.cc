b5a01c6776f9666d5dbb6cda80e34f0eba208b04
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2023-12-02 16:11:20 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -1,33 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-char c[101][101];
-int arr[101][101];
 
-int main() {
-    int h, w;
-    cin >> h >> w;
-    
-    memset(arr, -1, sizeof(arr));
-    
-    for (int i = 1; i <= h; i++){
-        for (int j = 1; j <= w; j++){
-            cin >> c[i][j];
-        }
-    }
-    for (int i = 1; i <= h; i++){
-        for (int j = 1; j <= w; j++){
-            if (c[i][j] == 'c'){
-                arr[i][j] = 0;
-            }
-            else if (arr[i][j - 1] >= 0){
-                arr[i][j] = arr[i][j - 1] + 1;
-            }
-        }
-    }
-    for (int i = 1; i <= h; i++){
-        for (int j = 1; j <= w; j++){
-            cout << arr[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+int h,w;
+int arr[104][104];
+
+int main(){
+	cin >> h >> w;
+	string s;
+	for(int i = 0 ; i < h ; i++){
+		cin >> s;
+		for(int j = 0 ; j < w ; j++){
+			if(s[j] == 'c') arr[i][j] = 0;
+			else arr[i][j] = -1;
+		}
+	}
+	
+	for(int i = 0 ; i < h ; i++){
+		for(int j = 0 ; j < w-1 ; j++){
+			if(arr[i][j] >= 0 && arr[i][j+1] != 0){
+				arr[i][j+1] = arr[i][j] +1; 
+			}
+		}
+	}
+	
+	for(int i = 0 ; i < h ; i++){
+		for(int j = 0 ; j < w ; j++){
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	
+	
+	return 0;
 }
