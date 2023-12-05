785023928b63b0f5b116472bc4b2b36507f89c50
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2023-12-05 17:24:10 +0900

    Update 교수가 된 현우.cc

--- 백준/파트2(DFS,BFS)/(다시보기)(91)3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/파트2(DFS,BFS)/(다시보기)(91)3474. 교수가 된 현우/교수가 된 현우.cc
@@ -3,9 +3,9 @@ using namespace std;
 int n, a;
 
 int main () {
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    cout.tie(NULL);
+    	ios_base::sync_with_stdio(false);
+    	cin.tie(NULL); 
+    	cout.tie(NULL);
     
 	cin >> n; 
 	for(int i = 0; i < n; i++){
