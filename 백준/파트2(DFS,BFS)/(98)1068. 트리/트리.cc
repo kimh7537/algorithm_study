6d21494f4b97e34a17ac9223852326a4ac9f7bdc
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-02-05 15:09:58 +0900

    Create 트리.cc

---
+++ 백준/파트2(DFS,BFS)/(98)1068. 트리/트리.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int temp, n, d, root;
+int child;
+vector<int> v[54];
+
+void dfs(int here){
+	if(v[here].size() == 0) child++;
+	if(v[here].size() == 1 && v[here][0] == d) child++;
+	
+	for(int there : v[here]){
+		if(there == d) continue;
+		dfs(there);
+	}
+	return;
+}
+ 
+
+int main(){
+	cin >> n;
+	
+	for(int i=0 ; i < n ; i++){
+		cin >> temp;
+		if(temp == -1){
+			root = i;
+			continue;
+		}
+		v[temp].push_back(i);
+	}
+	
+	cin >> d;
+	if(root == d){
+		cout << "0" << "\n";
+		return 0;
+	}
+	dfs(root);
+	cout << child << "\n";
+	
+	return 0;
+}
