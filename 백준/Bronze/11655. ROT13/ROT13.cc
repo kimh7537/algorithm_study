fc7baf4bcca73f78d430f7d640685eda4bde9646
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-02-09 00:58:45 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -4,15 +4,12 @@ string s;
 int main(){
     getline(cin, s); 
     for(int i = 0; i < s.size(); i++){
-        if('A' <= s[i] && s[i] <= 'Z'){
-            if(s[i] + 13 > 90) {
-            	s[i] = s[i] + 13 - 26; 
-            }
+        // 대문자인경우
+        if(s[i] >= 65 && s[i] < 97){
+            if(s[i] + 13 > 90) s[i] = s[i] + 13 - 26; 
             else s[i] = s[i] + 13;  
-        }else if('a' <= s[i] && s[i] <= 'z'){
-            if(s[i] + 13 > 122){
-            	s[i] = s[i] + 13 - 26; 
-            }
+        }else if(s[i] >= 97 && s[i] <= 122){
+            if(s[i] + 13 > 122)s[i] = s[i] + 13 - 26; 
             else s[i] = s[i] + 13;  
         }
         cout << s[i];  
