88f23b00710346009b322d6ffdba5da75a162f48
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-02-14 16:54:39 +0900

    Update 나는야 포켓몬 마스터 이다솜(2).cc

--- 백준/파트1/(9)1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜(2).cc
+++ 백준/파트1/(9)1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜(2).cc
@@ -1,36 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int n, m;
-string s;
+string s; 
 map<string, int> mp;
 map<int, string> mp2;
 string a[100004];
-
-int main(){
+int main(){  
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m;
-	for(int i = 0 ; i < n ; i++){
-		cin >> s;
-		
-		mp[s] = i+1;
-		mp2[i+1] = s;
-		//a[i + 1] = s;
-		//mp.insert({temp, i+1});
-		//mp2.insert({i+1, temp});
+	cin >> n >> m; 
+	for(int i = 0; i < n; i++){
+		cin >> s; 
+		mp[s] = i + 1;  
+		mp2[i + 1] = s; 
+        	a[i + 1] = s;
 	}
-	
-	for(int i = 0 ; i < m ; i++){
-		cin >> s;
+	for(int i = 0; i < m; i++){
+		cin >> s; 
 		if(atoi(s.c_str()) == 0){
 			cout << mp[s] << "\n";
-//			auto it = mp.find(temp);
-//			cout << (*it).second << "\n";
-		}else {
-			//cout << a[atoi(s.c_str())] << '\n';
-			cout << mp2[atoi(s.c_str())] << "\n";
+		}else{
+			cout << a[atoi(s.c_str())] << "\n"; 
+			//cout << mp2[atoi(s.c_str())] << "\n"; 
 		}
-	}
-	return 0;
+	} 
 }
+
