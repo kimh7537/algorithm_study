5c70d29ac1e81cef7b4782463dba771f4a50b7cd
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-02-19 13:53:32 +0900

    Create 주몽(2).cc

---
+++ 백준/파트1/(92)1940. 주몽/주몽(2).cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> 
+using namespace std;     
+int n, m, a[15001], cnt;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+  
+	cin >> n >> m; 
+	for(int i = 0; i < n; i++) cin >> a[i];  
+	//이 if문은 없어도됨. but, 설명을 위해.
+	if(m > 200000) cout << 0 << "\n";
+	else{
+		for(int i = 0; i < n; i++){
+			for(int j = i + 1; j < n; j++){
+				if(a[i] + a[j] == m)cnt++;
+			}
+		}
+		cout << cnt << "\n";
+	} 
+}
