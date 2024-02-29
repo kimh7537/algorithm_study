06028e5da1562c0e4cef0b5699e5f054227f4d75
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-02-29 14:45:23 +0900

    Update 곱셈(2).cc

--- 백준/파트1/(다시보기)(94)1629. 곱셈/곱셈(2).cc
+++ 백준/파트1/(다시보기)(94)1629. 곱셈/곱셈(2).cc
@@ -5,7 +5,7 @@ typedef long long ll;
 ll a, b, c;
 
 ll go(ll a, ll b){
-//	if(b == 1) return a;
+//	if(b == 1) return a; 이게 안되는 반레 (4, 1, 3) -> b가 1이라면 %c 연산이 발생하지 않고 바로 a를 반환함
 	if(b == 0) return 1;
 	ll ret = go(a, b/2); 
 	ret = (ret * ret) % c;
