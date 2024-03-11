9c955f778aef0d49e4c93b304479f0d85877f8fa
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-03-11 21:10:47 +0900

    Update 뮤탈리스크(2).cc

--- 백준/파트3/(다시보기)(5)12869. 뮤탈리스크/뮤탈리스크(2).cc
+++ 백준/파트3/(다시보기)(5)12869. 뮤탈리스크/뮤탈리스크(2).cc
@@ -42,8 +42,8 @@ int bfs(int a, int b, int c){
 int main(){
 	
 	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+        cin.tie(NULL);
+        cout.tie(NULL);
 	
 	cin >> n;
 	for(int i = 0 ; i < n ; i++){
