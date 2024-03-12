faa0d33783a16756948c8cabd1d423ae861cb27d
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-03-12 21:08:42 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 8 ms, Memory: 4680 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, k, ret, temp = 987654321;
+int visited[100004];
+
+
+void solve(int idx, int sum){
+	queue<pair<int,int>> q;
+	q.push({idx, sum});
+//	visited[sum] 
+	
+	while(q.size()){
+		tie(idx, sum) = q.front();
+		q.pop();
+		visited[sum] = 1;
+
+		if(sum == k && ret == 0){
+			ret++;
+			temp = idx;
+		}else if(sum == k && ret > 0 && idx == temp){
+			ret++;
+		}
+		if(idx > temp) continue;
+		
+		if(0 <= sum-1 && !visited[sum-1]){
+			q.push({idx+1, sum-1});
+		}
+		if(sum+1 <= 100000 && !visited[sum + 1]){
+			q.push({idx+1, sum+1});
+		}
+		if(sum*2 <= 100000 && !visited[sum * 2]){
+			q.push({idx+1, sum*2});
+		}
+	}
+	
+}
+
+int main(){
+	cin >> n >> k;
+	
+	solve(0, n);
+	
+	cout << temp << "\n";
+	cout << ret << "\n";
+	
+	return 0;
+}
