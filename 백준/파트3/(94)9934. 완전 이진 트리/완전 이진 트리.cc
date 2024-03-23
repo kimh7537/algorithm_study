89ad989db213862794b7eeb6732ef83b7b9bcf85
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-03-23 20:16:41 +0900

    Update 완전 이진 트리.cc

--- 백준/파트3/(94)9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/파트3/(94)9934. 완전 이진 트리/완전 이진 트리.cc
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
-using namespace std; 
-vector<int> ret[14];
-int n, a[1030];
-void go(int s, int e, int level){ 
-    if(s > e) return; 
-    if(s == e){ 
-        ret[level].push_back(a[s]); 
-        return;
-    }
-    int mid = (s + e) / 2;
-    ret[level].push_back(a[mid]);
-    go(s, mid - 1, level + 1); 
-    go(mid + 1, e, level + 1); 
-    return;
+using namespace std;
+
+int k, num, arr[1030], temp;
+vector<int> v[11];
+
+void go(int kk, int idx){
+	
+	if(kk == 1) {
+		v[1].push_back(arr[num/2]);
+		return;
+	}
+	int n = (int)pow(2, kk-1);
+	for(int i = temp ;; i += idx){
+		if(n == 0) break;
+		v[kk].push_back(arr[i]);
+		n--;
+	}
+	temp += idx / 2;
+	go(kk-1, idx*2);
 }
+
 int main(){
-    cin >> n;
-    int _end = (int)pow(2, n) - 1; 
-    for(int i = 0; i < _end; i++){
-        cin >> a[i]; 
-    }
-    go(0, _end, 1); 
-    for(int i = 1; i <= n; i++){
-        for(int j : ret[i]){
-            cout << j << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
+	cin >> k;
+	num = (int)pow(2, k) - 1;
+
+	for(int i = 0 ; i < num ; i++){
+		cin >> arr[i];
+	}
+
+	go(k, 2);
+	
+	for(int i = 1 ; i <= k ; i++){
+		for(auto it : v[i]){
+			cout << it << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
 }
-// 3
-// 1, 5
-// 0, 2, 4, 6
