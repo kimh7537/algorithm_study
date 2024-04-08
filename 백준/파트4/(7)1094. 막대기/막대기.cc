8eedc341231af4adcd48576bdcd1654c8c0e4315
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-04-08 19:20:23 +0900

    Update 막대기.cc

--- 백준/파트4/(7)1094. 막대기/막대기.cc
+++ 백준/파트4/(7)1094. 막대기/막대기.cc
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ret = 1;
-int main(){ 
-	cin >> n; 
-	while(n != 1){
-        if(n & 1) ret++; 
-        n /= 2;
+
+int x, cnt;
+int main(){
+	cin >> x;
+	int num = 1 << 6;
+	
+	for(int i = 0 ; i < 7 ; i++){
+		if(x & (1 << i)){
+			cnt++;
+		}
 	}
-	cout << ret << '\n';
-    return 0;
+	cout << cnt << "\n";
+	
+	return 0;
 }
