f68db580272b7a4053c858c450637cfa27828b07
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-04-09 17:36:23 +0900

    [Silver V] Title: 집합, Time: 732 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,16 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, x;
-char s[11];
-int main() {
-    scanf(" %d", &m);
-    for (int i = 0; i < m; i++) {
-        scanf(" %s %d", &s, &x); 
-        if (s[0]=='a' && s[1]=='d') n |= (1 << x);
-        else if (s[0] == 'r') n &= ~(1 << x);
-        else if (s[0] == 'c') printf("%d\n", (n&(1 << x)) ==0? 0: 1);
-        else if (s[0] == 't') n ^=(1<<x);
-        else if (s[0] == 'a' && s[1] == 'l') n = (1 << 21) - 1;
-        else n = 0;
-    }
+long long ret;
+int m, num;
+string s;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> m;
+
+	for(int i = 0 ; i < m ; i++){
+		cin >> s;
+		if(s != "all" && s != "empty"){
+			cin >> num;
+		}
+		
+		if(s == "add"){
+			ret |= (1 << num-1);
+		}else if(s == "remove"){
+			ret &= ~(1 << num-1); 
+		}else if(s == "check"){
+			if(ret & (1 << num-1)) cout << 1 << "\n";
+			else cout << 0 << "\n";
+		}else if(s == "toggle"){
+			ret ^= (1 << num-1);
+		}else if(s == "all"){
+			ret = (1 << 20) - 1;
+		}else if(s == "empty"){
+			ret = 0;
+		}
+	}
+	
+	
+	return 0;
 }
