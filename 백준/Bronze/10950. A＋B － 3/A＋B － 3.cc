ee393ac6cb2dd4da13d0ea1c0d46185beac7bafb
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2024-04-15 17:53:54 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a, b;
+
+int main(){
+	
+	cin >> n;
+	
+	for(int i = 0 ; i < n ; i++){
+		cin >> a >> b;
+		
+		cout << a + b << "\n";
+	}
+	
+	return 0;
+}
