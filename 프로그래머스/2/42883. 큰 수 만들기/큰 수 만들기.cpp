0b107d67006c0f1adbede6ed2d516c2f2b11b867
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2025-02-25 15:59:37 +0900

    [level 2] Title: 큰 수 만들기, Time: 1915.59 ms, Memory: 5.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, first, second, num = -1, idx;
+vector<int> v;
+
+string solution(string number, int k) {
+    string answer = "";
+
+    n = number.size();
+    first = 0, second = k;
+    
+    while(k > 0 && second < number.size()){
+        idx = 0;
+        num = -1;
+        for(int i = first ; i <= second ; i++){
+            if(number[i] - '0' >  num){
+                num = number[i] - '0';
+                idx = i;
+            }
+        }
+        k -= (idx - first);
+        v.push_back(idx);
+        first = idx + 1;
+        second = second + 1;
+    }
+    
+    for(auto it : v){
+        answer += number[it];
+        idx = it;
+    }
+    if(v.size() < number.size()){
+        if(k == 0) answer += number.substr(idx+1);
+    }
+    
+    return answer;
+}
