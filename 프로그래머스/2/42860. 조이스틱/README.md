644bf319f0dc86988e02a702373e216712f9a92b
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2025-03-14 19:02:38 +0900

    [level 2] Title: 조이스틱, Time: 3.98 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 4.17 MB, 시간: 3.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 14일 17:57:24
+2025년 03월 14일 19:02:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,24 +1,55 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <queue>
+#include <cmath>
 using namespace std;
 
-int result, length, mv, y;
+struct Data {
+    string s;
+    int d, i;
+    Data(string str, int distance, int idx) : s(str), d(distance), i(idx) {}
+};
 
 int solution(string name) {
-    length = name.size();
-    mv = length - 1;
+    int answer = 0;
+    string sbFirst(name.length(), 'A');
     
-    for(int x = 0 ; x < length ; x++){
-        result += min(name[x] - 'A', 'Z' - name[x] + 1);
+    queue<Data> que;
+    que.push(Data(sbFirst, 0, 0));
+    
+    while (!que.empty()) {
+        Data data = que.front();
+        que.pop();
         
-        y = x + 1;
-        while(y < length && name[y] == 'A'){
-            y++;
+        string temp = data.s;
+        
+        if (abs(name[data.i] - data.s[data.i]) > 13) { // 뒤로 움직이는 게 빠를 때
+            data.d += (26 - abs(name[data.i] - data.s[data.i]));
+        } else {
+            data.d += abs(name[data.i] - data.s[data.i]);
         }
-        mv = min(mv, min(x + x + (length -y), x + (length -y) + (length - y)));
+        
+        temp[data.i] = name[data.i]; // 문자 변경
+        data.s = temp;
+        
+        // 앞으로 전진
+        if (data.i + 1 == name.length()) {
+            que.push(Data(data.s, data.d + 1, 0));
+        } else {
+            que.push(Data(data.s, data.d + 1, data.i + 1));
+        }
+        
+        // 뒤로 후진
+        if (data.i - 1 == -1) {
+            que.push(Data(data.s, data.d + 1, name.length() - 1));
+        } else {
+            que.push(Data(data.s, data.d + 1, data.i - 1));
+        }
+        
+        // 정답 찾았을 때
+        if (data.s == name) {
+            answer = data.d;
+            break;
+        }
     }
-    
-    result += mv; 
-    
-    return result;
+    return answer;
 }
