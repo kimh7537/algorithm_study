e98c34500ab6d82546e9fc07cf2fddfc951824da
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2025-03-14 17:57:26 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+int result, length, mv, y;
+
 int solution(string name) {
-    int n = name.size();
-    int min_move = n - 1;  // 기본적으로 오른쪽으로 쭉 가는 경우
-    int total_change = 0;
+    length = name.size();
+    mv = length - 1;
     
-    // 문자 변경 횟수 계산
-    for (int i = 0; i < n; i++) {
-        // 알파벳 변경 횟수
-        total_change += min(name[i] - 'A', 'Z' - name[i] + 1);
+    for(int x = 0 ; x < length ; x++){
+        result += min(name[x] - 'A', 'Z' - name[x] + 1);
         
-        // 이동 경로 최적화
-        int next = i + 1;
-        while (next < n && name[next] == 'A') {
-            next++; // 연속된 A는 건너뛰기
+        y = x + 1;
+        while(y < length && name[y] == 'A'){
+            y++;
         }
-        min_move = min(min_move, i + n - next + min(i, n - next));
+        mv = min(mv, min(x + x + (length -y), x + (length -y) + (length - y)));
     }
     
-    return total_change + min_move;
+    result += mv; 
+    
+    return result;
 }
