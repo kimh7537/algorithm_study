ed112cc6040068520746f99bdbf949fe5caf64ed
kimh7537 <142154451+kimh7537@users.noreply.github.com>
2026-04-25 20:21:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.29 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int dy[4] = {0, 0, 1, -1};
+int dx[4] = {1, -1, 0, 0};
+int visited[104][104] = {0,};
+
+void bfs(vector<vector<int>> maps){
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    visited[0][0] += 1;
+    
+    while(q.size() > 0){
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        
+        for(int i = 0 ; i < 4 ; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            
+            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+            if(visited[ny][nx] > 0) continue;
+            if(maps[ny][nx] == 0) continue;
+            q.push({ny, nx});
+            visited[ny][nx] = visited[y][x] + 1;
+        }
+    }
+    
+    // for(int i = 0 ; i < n ; i++){
+    //     for(int j = 0 ; j < m ; j++){
+    //         cout << visited[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+}
+
+int solution(vector<vector<int> > maps){
+    int answer = 0;
+    n = maps.size();
+    m = maps[0].size();
+    
+    bfs(maps);
+    
+    if(visited[n-1][m-1] == 0){
+        answer = -1;
+    }else if(visited[n-1][m-1] >= 1){
+        answer = visited[n-1][m-1];
+    }
+    
+    return answer;
+}
